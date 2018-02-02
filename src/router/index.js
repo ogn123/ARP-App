f494053e6aab5559172fda0bb7fa26d6e2fdbb8e
sqh17 <852104785@qq.com>
2018-02-02 15:01:29 +0800

    修改路由，按需下载

--- src/router/index.js
+++ src/router/index.js
@@ -1,71 +1,95 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Index from '../pages/Index/Index'
 
 
-
+// 首页
+// import Index from '../pages/Index/Index'
+const Index = resolve => require(['../pages/Index/Index'], resolve)
 //消息
-import Message from '../pages/messages/message'
-
+// import Message from '../pages/messages/message'
+const Message = resolve => require(['../pages/messages/message'], resolve)
 
 //应用
-import Application from '../pages/application/application'
-
+// import Application from '../pages/application/application'
+const Application = resolve => require(['../pages/application/application'], resolve)
 // 借款
 //我的借款
-import myLoan from '../pages/application/loan/myLoan'
+// import myLoan from '../pages/application/loan/myLoan'
+const myLoan = resolve => require(['../pages/application/loan/myLoan'], resolve)
+
 //我的借款
-import paymentDetails from '../pages/application/loan/paymentDetails'
+// import paymentDetails from '../pages/application/loan/paymentDetails'
+const paymentDetails = resolve => require(['../pages/application/loan/paymentDetails'], resolve)
 //我信息查询
-import informationSearch from '../pages/application/informationSearch/informationSearch'
+// import informationSearch from '../pages/application/informationSearch/informationSearch'
+const informationSearch = resolve => require(['../pages/application/informationSearch/informationSearch'], resolve)
 
 // 新增发文
-import emailArticle from '../pages/application/emailArticle/emailArticle'
+// import emailArticle from '../pages/application/emailArticle/emailArticle'
+const emailArticle = resolve => require(['../pages/application/emailArticle/emailArticle'], resolve)
 // 新增公文详情
-import documentDetail from '../pages/application/emailArticle/documentDetail'
+// import documentDetail from '../pages/application/emailArticle/documentDetail'
+const documentDetail = resolve => require(['../pages/application/emailArticle/documentDetail'], resolve)
 //新增填写回执
-import WriteHZ from '../pages/application/emailArticle/writeHZ'
+// import WriteHZ from '../pages/application/emailArticle/writeHZ'
+const WriteHZ = resolve => require(['../pages/application/emailArticle/writeHZ'], resolve)
 
 // 报销
 //我的报销
-import myAccount from '../pages/application/account/myAccount'
+// import myAccount from '../pages/application/account/myAccount'
+const myAccount = resolve => require(['../pages/application/account/myAccount'], resolve)
 
 // 我的信息
-import myInformation from '../pages/Mine/myInformation/myInformation'
+// import myInformation from '../pages/Mine/myInformation/myInformation'
+const myInformation = resolve => require(['../pages/Mine/myInformation/myInformation'], resolve)
 
 // 我的资产
-import myProperty from '../pages/Mine/myProperty/myProperty'
+// import myProperty from '../pages/Mine/myProperty/myProperty'
+const myProperty = resolve => require(['../pages/Mine/myProperty/myProperty'], resolve)
 // 历史记录
-import historyRecord from '../pages/Mine/myProperty/historyRecord'
+// import historyRecord from '../pages/Mine/myProperty/historyRecord'
+const historyRecord = resolve => require(['../pages/Mine/myProperty/historyRecord'], resolve)
 // 薪酬
 //我的薪酬-密码
-import enterPassword from '../pages/Mine/mySalary/enterPassword'
+// import enterPassword from '../pages/Mine/mySalary/enterPassword'
+const enterPassword = resolve => require(['../pages/Mine/mySalary/enterPassword'], resolve)
 //我的薪酬
-import mySalary from '../pages/Mine/mySalary/mySalary'
+// import mySalary from '../pages/Mine/mySalary/mySalary'
+const mySalary = resolve => require(['../pages/Mine/mySalary/mySalary'], resolve)
 //工资详情
-import salaryDetail from '../pages/Mine/mySalary/salaryDetail'
+// import salaryDetail from '../pages/Mine/mySalary/salaryDetail'
+const salaryDetail = resolve => require(['../pages/Mine/mySalary/salaryDetail'], resolve)
 
 // 左右滑动
 import abc from '../pages/myProject/abc'
 // 我的项目
-import myProject from '../pages/Mine/myProject/myProject'
+// import myProject from '../pages/Mine/myProject/myProject'
+const myProject = resolve => require(['../pages/Mine/myProject/myProject'], resolve)
 // 项目详情
-import projectDetail from '../pages/Mine/myProject/projectDetail'
+// import projectDetail from '../pages/Mine/myProject/projectDetail'
+const projectDetail = resolve => require(['../pages/Mine/myProject/projectDetail'], resolve)
 // 项目预算
-import projectBudget from '../pages/Mine/myProject/projectBudget'
+// import projectBudget from '../pages/Mine/myProject/projectBudget'
+const projectBudget = resolve => require(['../pages/Mine/myProject/projectBudget'], resolve)
 // 项目团队
-import projectGroup from '../pages/Mine/myProject/projectGroup'
+// import projectGroup from '../pages/Mine/myProject/projectGroup'
+const projectGroup = resolve => require(['../pages/Mine/myProject/projectGroup'], resolve)
 // 核算账号
-import checkAccountNumber from '../pages/Mine/myProject/checkAccountNumber'
+// import checkAccountNumber from '../pages/Mine/myProject/checkAccountNumber'
+const checkAccountNumber = resolve => require(['../pages/Mine/myProject/checkAccountNumber'], resolve)
 
 //通讯录
-import Contact from '../pages/contacts/contact'
+// import Contact from '../pages/contacts/contact'
+const Contact = resolve => require(['../pages/contacts/contact'], resolve)
 //通讯录下一级
-import SubContact from '../pages/contacts/subContact/subContact'
+// import SubContact from '../pages/contacts/subContact/subContact'
+const SubContact = resolve => require(['../pages/contacts/subContact/subContact'], resolve)
 //通讯录下下一级
-import SubContact1 from '../pages/contacts/subContact1/subContact1'
+// import SubContact1 from '../pages/contacts/subContact1/subContact1'
+const SubContact1 = resolve => require(['../pages/contacts/subContact1/subContact1'], resolve)
 //个人信息详细页
-import PersonDetail from '../pages/contacts/personDetail/personDetail'
+// import PersonDetail from '../pages/contacts/personDetail/personDetail'
+const PersonDetail = resolve => require(['../pages/contacts/personDetail/personDetail'], resolve)
 
 import Mine from '../pages/Mine/Mine'
 //引入头部组件
